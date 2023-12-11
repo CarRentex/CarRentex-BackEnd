fa6587b6fd9c2c97ad45bdda8141a3c5f60510cd
Burak Karlıdağ <burakkarlidag@192.168.1.100>
2023-12-11 17:06:35 +0300

    Spring starter

---
+++ RentaCar/src/main/java/com/Tobeto/RentaCar/RentaCarApplication.java
@@ -0,0 +1,13 @@
+package com.Tobeto.RentaCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentaCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentaCarApplication.class, args);
+	}
+
+}
---
+++ RentaCar/src/test/java/com/Tobeto/RentaCar/RentaCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tobeto.RentaCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentaCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
