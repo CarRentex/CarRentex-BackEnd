ffebf951baccd93cebaf8065bc433d4672101cb9
Burak Karlıdağ <burakkarlidag@192.168.1.100>
2023-12-13 18:28:02 +0300

    bug solved and dto , manager controller update

--- src/main/java/com/Tobeto/RentaCar/service/concretes/BrandManager.java
+++ src/main/java/com/Tobeto/RentaCar/service/concretes/BrandManager.java
@@ -1,7 +1,40 @@
 package com.Tobeto.RentaCar.service.concretes;
 
+import com.Tobeto.RentaCar.service.abstracts.BrandService;
+import com.Tobeto.RentaCar.service.dto.request.Brand.AddBrandRequest;
+import com.Tobeto.RentaCar.service.dto.request.Brand.UpdateBrandRequest;
+import com.Tobeto.RentaCar.service.dto.response.Brand.GetBrandListResponse;
+import com.Tobeto.RentaCar.service.dto.response.Brand.GetBrandResponse;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@AllArgsConstructor
 @Service
-public class BrandManager {
+public class BrandManager implements BrandService {
+    @Override
+    public void add(AddBrandRequest addBrandRequest) {
+
+    }
+
+    @Override
+    public GetBrandResponse getById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<GetBrandListResponse> getAll() {
+        return null;
+    }
+
+    @Override
+    public void update(UpdateBrandRequest updateBrandRequest, int id) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
 }
