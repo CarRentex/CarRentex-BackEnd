4c17102724af0c33716df7c6dee908f485e48183
Burak Karlıdağ <burakkarlidag@192.168.1.100>
2023-12-13 16:38:01 +0300

    swager connections

--- src/main/java/com/Tobeto/RentaCar/controllers/BrandController.java
+++ src/main/java/com/Tobeto/RentaCar/controllers/BrandController.java
@@ -1,5 +1,14 @@
 package com.Tobeto.RentaCar.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/brand")
 public class BrandController {
-    //burak
+    @GetMapping
+    public  void Brand() {
+        return;
+    }
 }
