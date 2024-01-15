5d6fa7343f6b37ff204c45caefc983c6ff5ccdf6
zeysev <147267051+zeysev@users.noreply.github.com>
2024-01-15 16:49:28 +0300

    .

--- src/main/java/com/Tobeto/RentaCar/rules/color/ColorBusinessRuleService.java
+++
@@ -1,9 +0,0 @@
-package com.Tobeto.RentaCar.rules.color;
-
-import com.Tobeto.RentaCar.service.dto.request.Color.AddColorRequest;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface ColorBusinessRuleService {
-    void create (AddColorRequest addColorRequest);
-}
--- src/main/java/com/Tobeto/RentaCar/rules/model/ModelBusinessRuleService.java
+++
@@ -1,7 +0,0 @@
-package com.Tobeto.RentaCar.rules.model;
-
-import com.Tobeto.RentaCar.service.dto.request.Model.AddModelRequest;
-
-public interface ModelBusinessRuleService {
-    void create (AddModelRequest addModelRequest);
-}
